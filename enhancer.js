283a5e55857b5d3a19dffbdbee44527e827976be
Vito Van <awesomevito@live.com>
2015-11-23 18:40:55 +0800

    done, time to rename repo

--- enhancer.js
+++ enhancer.js
@@ -20,6 +20,8 @@ function fillComments(jqDom){
     });
 }
 
+//loading
+showSpinner();
 //Get comments from current page
 fillComments($('body'));
 
@@ -92,11 +94,21 @@ function moveComment(comment,parent){
     }
 }
 
+function showSpinner(){
+    var commentBox = $('#Main>div.box:nth(1)');
+    $('body').append('<style>.spinner{width:40px;height:40px;position:relative;margin:100px auto}.double-bounce1,.double-bounce2{width:100%;height:100%;border-radius:50%;background-color:#333;opacity:.6;position:absolute;top:0;left:0;-webkit-animation:sk-bounce 2.0s infinite ease-in-out;animation:sk-bounce 2.0s infinite ease-in-out}.double-bounce2{-webkit-animation-delay:-1.0s;animation-delay:-1.0s}@-webkit-keyframes sk-bounce{0%,100%{-webkit-transform:scale(0.0)}50%{-webkit-transform:scale(1.0)}}@keyframes sk-bounce{0%,100%{transform:scale(0.0);-webkit-transform:scale(0.0)}50%{transform:scale(1.0);-webkit-transform:scale(1.0)}}</style>');
+    $('<div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>').insertBefore(commentBox);
+    commentBox.hide();
+}
 function reArrangeComments(){
     $('div.inner:has(a[href^="/t/"].page_normal)').remove();
+    var commentBox = $('#Main>div.box:nth(1)');
     $.each(comments,function(i,o){
-        moveComment(o,$('#Main>div.box'));
+        moveComment(o,commentBox);
     });
     $('div[id^="r_"]>table>tbody>tr>td:first-child').attr('width','20');
+    $('body').append('<style>.cell{border-bottom:none;}div[id^="r_"] img.avatar{width:20px;border-radius:50%;}div[id^="r_"]>div{margin-left: 21px;}div.box>div[id^="r_"]{border-bottom: 1px solid #E2E2E2;}</style>');
+    commentBox.show();
+    //removeSpinner
+    $('.spinner').remove();
 }
-$('body').append('<style>.cell{border-bottom:none;}div[id^="r_"] img.avatar{width:20px;border-radius:50%;}div[id^="r_"]>div{margin-left: 21px;}div.box>div[id^="r_"]{border-bottom: 1px solid #E2E2E2;}</style>');
