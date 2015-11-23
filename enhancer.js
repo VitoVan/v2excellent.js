7fceccccc96ec9e0990d1da285262e8e8560ddfa
Vito Van <awesomevito@live.com>
2015-11-23 16:03:15 +0800

    making tree

--- enhancer.js
+++ enhancer.js
@@ -1,20 +1,23 @@
 var comments = [];
 function fillComments(jqDom){
     jqDom.find('div[id^="r_"]').each(function(i,o){
-        comments[parseInt($(o).find('span.no').text())] = {user: $(o).find('strong>a').text(),content: $(o).find('div.reply_content').text()};
+        var cmno = parseInt($(o).find('span.no').text());
+        comments[cmno] =
+            {
+                no: cmno,
+                user: $(o).find('strong>a').text(),
+                content: $(o).find('div.reply_content').text(),
+                mentioned: (function(){
+                    var mentionedNames = [];
+                    $(o).find('div.reply_content>a[href^="/member/"]:not("dark")').each(function(i,o){
+                        mentionedNames.push(o.innerHTML);
+                    });
+                    return mentionedNames;
+                }())
+            };
     });
 }
 
-function findPreComments(user,current_no){
-    var preComments = []
-    for(var i=current_no;i>0;i--){
-        var cc = comments[i];
-        if(cc.user === user){
-            preComments.push(cc);
-        }
-    }
-    return preComments;
-}
 //Get comments from current page
 fillComments($('body'));
 
@@ -26,37 +29,80 @@ $('div.inner>a[href^="/t/"].page_normal').each(function(i,o){
     });
 });
 
-//build reply_stack
-var reply_stack = $(document.createElement('reply_stack')).on('mouseenter click touchend',function(e){
-    if($(e.currentTarget).hasClass('shown')){
-        return;
-    }
-    //get who is @ed
-    $(e.currentTarget).parent().find('a[href^="/member"]').not('.dark').each(function(i,o){
-        //get comments of @ed user
-        var currentNo = parseInt($(o).parent().parent().find('span.no').text());
-        var preComments = findPreComments(o.innerHTML, currentNo);
-        $(e.currentTarget).addClass('shown');
-        if(preComments.length>0){
-            //show'em
-            $(e.currentTarget).append(preComments[0].user + ': ' +
-                                      preComments[0].content + '<hr>');
-        }else{
-            $(e.currentTarget).append('No Comments');            
+//Get root comments
+function findRootComments(){
+    var rootComments = [];
+    for(var i=1,l=comments.length;i<comments.length;i++){
+        var cc = comments[i];
+        if(cc.mentioned.length === 0){
+            rootComments.push(cc);
         }
-    });
-});
-
-//add reply stack styles
-$('body').append('<style>reply_stack:before{content: "^";font-size: 9px;line-height: 9px;font-weight: 500;border-radius: 10px;display: inline-block;background-color: #f0f0f0;color: #ccc;padding: 2px 5px 2px 5px;float: right;width: 10px;text-align: center;}reply_stack{display:none;}reply_stack.shown{display: block !important;background: #000;border-radius: 3px;padding: 5px 5px 5px 14px;color: #E2E2E2;}reply_stack.shown:before {display: none;}reply_stack>hr:last-child{display:none;}</style>');
-
-$('div[id^="r_"]').on('mouseenter click touchend',function(e){
-    $(e.currentTarget).find('reply_stack').show();
-});
-$('div[id^="r_"]').mouseleave(function(e){
-    var rs = $(e.currentTarget).find('reply_stack');
-    if(!rs.hasClass('shown')){
-        rs.hide();
     }
-});
-$('div.reply_content:has(a[href^="/member/"])').append(reply_stack);
+    return rootComments;
+}
+
+//Get sub comments
+function findSubComments(comment){
+    var subComments = [];
+    //To say, if a user commented on the board after he/she is mentioned, if he/she did, this may raise a new conversation.
+    var USER_RECOMMENTED = false;
+    for(var i=comment.no+1,l=comments.length;i<comments.length;i++){
+        var cc = comments[i];
+        if(comment.user === cc.user){
+            USER_RECOMMENTED = true;
+        }
+        //If a user raised a new conversation, then we won't follow this comment anymore
+        if(USER_RECOMMENTED === true){
+            //We only need to find the comment which mentioned each other
+            if($.inArray(cc.user,comment.mentioned) !== -1 && $.inArray(comment.user,cc.mentioned) !== -1){
+                subComments.push(cc);
+                //Then break
+                break;
+            }
+        }else{
+            if($.inArray(comment.user,cc.mentioned) !== -1){
+                subComments.push(cc);
+            }
+        }
+    }
+    return subComments;
+}
+
+//Get sub comments, recursively
+function findSubCommentsR(comment){
+    var subComments = findSubComments(comment);
+    comment.subComments = subComments;
+    $.each(subComments, function(i, o){
+        findSubCommentsR(o);
+    });
+    return comment;
+}
+
+//Gen comments tree
+function genTree(){
+    var tree = [];
+    $.each(findRootComments(),function(i,o){
+        tree.push(findSubCommentsR(o));
+    });
+    return tree;
+}
+
+//Print Comment
+function printComment(comment,gap){
+    console.log(gap + comment.no + ' - ' + comment.user + ' - ' + comment.content);
+}
+
+//Print Comment, recursively
+function printCommentR(comment,gap){
+    if(!gap){gap='--';}
+    printComment(comment,gap);
+    gap += '--';
+    $.each(comment.subComments,function(i,o){
+        printCommentR(o,gap);
+    });
+}
+
+//Print Tree
+function printTree(tree){
+    
+}
