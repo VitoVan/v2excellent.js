7c5bec0858902d2e6880daab7e69e1cd4f71f545
fiyeck <57694411+fiyeck@users.noreply.github.com>
2020-01-02 10:58:51 +0800

    修复当前浏览器语言判断不准确

--- v2excellent.js
+++ v2excellent.js
@@ -49,7 +49,7 @@ POST_PROCESS_FUNCS.push(function markOp() {
     all_elem = document.getElementsByClassName('dark');
     for (var i = 0; i < all_elem.length; i++) {
       if (all_elem[i].innerHTML == lzname) {
-        var opWord = language === 'zh_CN' ? '楼主' : 'OP';
+        var opWord = language === 'zh-cn' ? '楼主' : 'OP';
         all_elem[i].innerHTML += ' <font color=green>[' + opWord + ']</font>';
       }
       //为回复所有人做准备
@@ -74,7 +74,7 @@ function postProcess() {
   });
 }
 
-var language = window.navigator.userLanguage || window.navigator.language;
+var language = (window.navigator.userLanguage || window.navigator.language).toLowerCase();
 
 var currentLocation = location.href;
 //If this is the thread page
