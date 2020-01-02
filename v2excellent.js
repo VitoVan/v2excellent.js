98ce919658ff8e539d6cdabbc24de3258a0a641d
Vito Van <awesomevito@live.com>
2020-01-02 13:50:02 +0800

    chore: bump version to 1.1.7

--- v2excellent.js
+++ v2excellent.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           V2EXcellent.js
 // @namespace      http://vitovan.github.io/v2excellent.js/
-// @version        1.1.6
+// @version        1.1.7
 // @description    A Better V2EX
 // @author         VitoVan
 // @include        http*://*v2ex.com/*
